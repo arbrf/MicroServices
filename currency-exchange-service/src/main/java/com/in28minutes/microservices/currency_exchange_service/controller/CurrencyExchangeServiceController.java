f4083ff4037550a925d3539a9db5ead110bd483e
Ibrahim <shaikmohammadibrahim311@gmail.com>
2025-07-06 10:20:38 +0530

    initial commit

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CurrencyExchangeServiceController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.microservices.currency_exchange_service.controller;
+
+import com.in28minutes.microservices.currency_exchange_service.pojo.CurrencyExchange;
+import com.in28minutes.microservices.currency_exchange_service.pojo.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeServiceController {
+    @Autowired
+    private Environment environment;
+    @GetMapping("currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValues
+            (@PathVariable String from,@PathVariable String to){
+        String env=environment.getProperty("local.server.port");
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50), env);
+        return currencyExchange;
+
+    }
+}
---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/pojo/CurrencyExchange.java
@@ -0,0 +1,62 @@
+package com.in28minutes.microservices.currency_exchange_service.pojo;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private Long id;
+    private  String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private String Environnment;
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple,String  environnment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.Environnment=environnment;
+    }
+
+    public CurrencyExchange() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironnment() {
+        return Environnment;
+    }
+
+    public void setEnvironnment(String environnment) {
+        Environnment = environnment;
+    }
+}
