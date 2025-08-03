c871d6d03f5b8ba84adec759969b32c868301964
Ibrahim <shaikmohammadibrahim311@gmail.com>
2025-08-03 11:45:26 +0530

    Getting Stated with docker

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.microservices.currency_exchange_service.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,15 +14,18 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CircuitBreakerController {
     Logger logger= LoggerFactory.getLogger(CircuitBreakerController.class);
-    int callCount=0;
+    Integer callCount=0;
 
     @GetMapping("sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "fallbackResponse")
+    //@Retry(name = "sample-api", fallbackMethod = "fallbackResponse")
+    //@CircuitBreaker(name = "sample-api", fallbackMethod = "fallbackResponse")
+    @Bulkhead(name="default")
+    //@RateLimiter(name = "default")
     public String sampleApi(){
         logger.info("The Sample API called {}", ++callCount);
-        ResponseEntity<String> stringResponseEntity = new RestTemplate()
-                .getForEntity("http://localhost:8080/sample-dummy", String.class);
-        return stringResponseEntity.getBody();
+       /* ResponseEntity<String> stringResponseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8080/sample-dummy", String.class);*/
+        return "sample-api  "+callCount.toString();
     }
 
     public String fallbackResponse(Exception ex) {
